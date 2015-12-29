540364fc9429d0d26370a6175881890c147dd19e
AlexEntersis <entersisresto@i.ua>
2015-12-29 22:38:55 +0200

    Update wsgi.py

--- WFSParser/wsgi.py
+++ WFSParser/wsgi.py
@@ -1,17 +1,8 @@
-"""
-WSGI config for WFSParser proje
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/"""
 import os
-import django
-django.setup
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WFSParser.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WFSParser.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
-
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
