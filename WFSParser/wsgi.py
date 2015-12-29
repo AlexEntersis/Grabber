be33c53310ba4f540ce1f5bd552d55f04269f5bc
Entersis <alexentersis@mail.ua>
2015-12-29 22:21:40 +0200

    WSGI

--- WFSParser/wsgi.py
+++ WFSParser/wsgi.py
@@ -5,9 +5,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/"""
 import os
+import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
+
+django.setup
